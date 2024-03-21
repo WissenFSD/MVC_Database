87a328118c25296bc855196e296215090ed4d116
Admin <Admin@DESKTOP-5789TGK>
2024-03-21 20:02:11 +0300

    Add project files.

---
+++ MVC_Database/Authentication.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MVC_Database;
+
+public partial class Authentication
+{
+    public int Id { get; set; }
+
+    public string? Username { get; set; }
+
+    public string? Password { get; set; }
+
+    public virtual ICollection<Autorization> Autorizations { get; set; } = new List<Autorization>();
+}
---
+++ MVC_Database/Autorization.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MVC_Database;
+
+public partial class Autorization
+{
+    public int Id { get; set; }
+
+    public int AuthId { get; set; }
+
+    public string? Controller { get; set; }
+
+    public string? Action { get; set; }
+
+    public virtual Authentication Auth { get; set; } = null!;
+}
